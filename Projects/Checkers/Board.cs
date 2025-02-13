a2214060b72d73072a5eb6987ac3d8c89aa8d726
Rhinereco <llsrhine0607@163.com>
2025-02-12 22:18:34 -0500

    Update Board.cs

--- Projects/Checkers/Board.cs
+++ Projects/Checkers/Board.cs
@@ -2,18 +2,21 @@
 
 public class Board
 {
-	public List<Piece> Pieces { get; }
+	public List<Piece> Pieces { get; }//read-only, stores all pieces currently on the board
 
-	public Piece? Aggressor { get; set; }
+	public Piece? Aggressor { get; set; }//represents the piece that must capture another piece
 
 	public Piece? this[int x, int y] =>
-		Pieces.FirstOrDefault(piece => piece.X == x && piece.Y == y);
+		Pieces.FirstOrDefault(piece => piece.X == x && piece.Y == y);//indexer to find a piece based on its coordinates
 
+	//Board constructor 
+	//Initializes the pieces on the board, with 12 pieces for each side
 	public Board()
 	{
 		Aggressor = null;
 		Pieces = new List<Piece>
 			{
+				//initialize pieces' positions.
 				new() { NotationPosition ="A3", Color = Black},
 				new() { NotationPosition ="A1", Color = Black},
 				new() { NotationPosition ="B2", Color = Black},
@@ -42,27 +45,33 @@ public class Board
 			};
 	}
 
-	public static string ToPositionNotationString(int x, int y)
+	public static string ToPositionNotationString(int x, int y)//Converts board coordinates into a string representation
 	{
 		if (!IsValidPosition(x, y)) throw new ArgumentException("Not a valid position!");
 		return $"{(char)('A' + x)}{y + 1}";
 	}
-
+	
+	//Parses a board position string like "A1" into a coordinate tuple (int X, int Y)
 	public static (int X, int Y) ParsePositionNotation(string notation)
 	{
 		if (notation is null) throw new ArgumentNullException(nameof(notation));
 		notation = notation.Trim().ToUpper();
-		if (notation.Length is not 2 ||
+		////check if the string is a valid board position representation
+		if (notation.Length is not 2 || 
 			notation[0] < 'A' || 'H' < notation[0] ||
 			notation[1] < '1' || '8' < notation[1])
 			throw new FormatException($@"{nameof(notation)} ""{notation}"" is not valid");
 		return (notation[0] - 'A', notation[1] - '1');
 	}
 
+	//check if the piece is the region of the 8*8 board
 	public static bool IsValidPosition(int x, int y) =>
 		0 <= x && x < 8 &&
 		0 <= y && y < 8;
 
+	//Find the closest pair of opposing pieces
+	//point1(x1,y1),point2(x2,y2)
+	//squared Euclidean distance =(x2-x1)^2+(y2-y1)^2
 	public (Piece A, Piece B) GetClosestRivalPieces(PieceColor priorityColor)
 	{
 		double minDistanceSquared = double.MaxValue;
@@ -83,11 +92,13 @@ public class Board
 		return closestRivals;
 	}
 
+	//Retrieves all possible moves for the specified color
 	public List<Move> GetPossibleMoves(PieceColor color)
 	{
 		List<Move> moves = new();
 		if (Aggressor is not null)
 		{
+			//Illegal move: current player is attempting to move a piece that is not the Aggressor
 			if (Aggressor.Color != color)
 			{
 				throw new Exception($"{nameof(Aggressor)} is not null && {nameof(Aggressor)}.{nameof(Aggressor.Color)} != {nameof(color)}");
@@ -98,6 +109,7 @@ public class Board
 		{
 			foreach (Piece piece in Pieces.Where(piece => piece.Color == color))
 			{
+				//Calculates possible moves for each piece on the board and adds them to the moves list
 				moves.AddRange(GetPossibleMoves(piece));
 			}
 		}
@@ -124,9 +136,10 @@ public class Board
 			(int X, int Y) target = (piece.X + dx, piece.Y + dy);
 			if (!IsValidPosition(target.X, target.Y)) return;
 			PieceColor? targetColor = this[target.X, target.Y]?.Color;
-			if (targetColor is null)
+			if (targetColor is null)//check if target position is null
 			{
 				if (!IsValidPosition(target.X, target.Y)) return;
+				//Creates a new move object representing a move from the current piece position to the target position
 				Move newMove = new(piece, target);
 				moves.Add(newMove);
 			}
@@ -143,6 +156,8 @@ public class Board
 	}
 
 	/// <summary>Returns a <see cref="Move"/> if <paramref name="from"/>-&gt;<paramref name="to"/> is valid or null if not.</summary>
+	//check if move is valid
+	//color is current player's piece color
 	public Move? ValidateMove(PieceColor color, (int X, int Y) from, (int X, int Y) to)
 	{
 		Piece? piece = this[from.X, from.Y];
@@ -150,6 +165,7 @@ public class Board
 		{
 			return null;
 		}
+		//iterate every legal Move object to find the match move
 		foreach (Move move in GetPossibleMoves(color))
 		{
 			if ((move.PieceToMove.X, move.PieceToMove.Y) == from && move.To == to)
