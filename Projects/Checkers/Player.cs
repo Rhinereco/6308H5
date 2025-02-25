6ef87f51d58e13040faf690f9630e4f784f1cceb
Rhinereco <llsrhine0607@163.com>
2025-02-25 01:06:03 -0500

    3A1B

--- Projects/Checkers/Player.cs
+++ Projects/Checkers/Player.cs
@@ -4,6 +4,7 @@ public class Player
 {
 	public int step_count = 1;
 	public int undo_count = 3;//3 undo chances per game
+	public bool switchTurnUsed = false;
 	public bool IsHuman { get; }
 	public PieceColor Color { get; }
 
@@ -15,4 +16,7 @@ public class Player
 
 	public bool CanUndo() => undo_count > 0; // check if player still can undo this game
     public void UseUndo() => undo_count--; // use an undo chance
+
+	public bool CanSwitchTurn() => !switchTurnUsed; // only can force to switch turn once per game 
+    public void UseSwitchTurn() => switchTurnUsed = true; //sign for chance used
 }
