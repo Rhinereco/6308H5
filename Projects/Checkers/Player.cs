6ee9584ee8fad83f9acc06505df9bbd267d1667a
Rhinereco <llsrhine0607@163.com>
2025-02-24 19:22:24 -0500

    3A1B
    //RequirementA-4: Pieces can move in all 8 directions
    //RequirementA-5: Player can undo a move (Maybe have only 3 undos per game)
    //RequirementA-6: Player can have an extra turn to use at any point in the game
    //RequirementB-4: Stamina System

--- Projects/Checkers/Player.cs
+++ Projects/Checkers/Player.cs
@@ -3,6 +3,7 @@
 public class Player
 {
 	public int step_count = 1;
+	public int undo_count = 3;//3 undo chances per game
 	public bool IsHuman { get; }
 	public PieceColor Color { get; }
 
@@ -11,4 +12,7 @@ public class Player
 		IsHuman = isHuman;
 		Color = color;
 	}
+
+	public bool CanUndo() => undo_count > 0; // check if player still can undo this game
+    public void UseUndo() => undo_count--; // use an undo chance
 }
